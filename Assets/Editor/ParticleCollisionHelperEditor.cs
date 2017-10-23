c5e9e0f043bdf2504af1802a604280bb819e0d38
John McElmurray <johnmce@microsoft.com>
2017-10-23 12:09:37 -0700

    Replaced uGUI UI with inspector UI on the script itself

---
+++ Assets/Editor/ParticleCollisionHelperEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ParticleCollisionHelper))]
+public class ParticleCollisionHelperEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        ParticleCollisionHelper phc = target as ParticleCollisionHelper;
+
+        // Show default inspector property editor
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Play"))
+        {
+            phc.Play();
+        }
+
+        if (GUILayout.Button("Pause"))
+        {
+            phc.Pause();
+        }
+    }
+}
